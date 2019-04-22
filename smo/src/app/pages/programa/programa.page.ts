a96d43100dd8c29cb0ba8e4fa31ecd48f0a29669
Jesús <jesus_mx12@hotmail.com>
2019-04-22 00:44:38 -0500

    Programa general agregado
    Se agrego el programa general con pendientes

--- smo/src/app/pages/programa/programa.page.ts
+++ smo/src/app/pages/programa/programa.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ActividadesService } from 'src/app/services/actividades.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-programa',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./programa.page.scss'],
 })
 export class ProgramaPage implements OnInit {
-
-  constructor() { }
+  variable:string="";
+  constructor(
+    public modalCtrl: ModalController,
+    private _as: ActividadesService,
+    private iab: InAppBrowser
+  ) { 
+  }
 
   ngOnInit() {
+    this._as.pagina = 0;
+    this._as.dias = [];
+    this._as.cargar_todos().then(
+      result => console.log("Ya hay datos",result)
+    );
   }
+
+
+ 
 
 }
---
+++ smo/src/app/services/actividades.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from "../config/url.servicios";
+import { AjustesService } from './ajustes.service';
+import { DatosService } from './datos.service';
+import 'rxjs/add/operator/map';
+import { promise } from 'protractor';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActividadesService {
+  pagina: number = 0;
+  dia_actual: string;
+  dias: IDia[];
+  constructor(
+    private http: HttpClient,
+    private _as: AjustesService,
+    private _ds: DatosService,
+  ) { }
+
+
+
+  async cargar_todos() {
+      this._as.presentLoading("Cargando...");
+      let url = await URL_SERVICIOS + "/minutoxminuto.php?todos&pagina=" + this.pagina;
+      let promesa = this.http.get<InterfaceActividades>(url)
+        .toPromise()
+        .then(data => {
+          this.dias = data.dias;
+          this.dia_actual=data.diaactual;
+          this.pagina = this.pagina += 1;
+          this._as.loading.dismiss();
+          return promesa;
+        })
+        .catch(error => {
+          this._as.loading.dismiss();
+          return Promise.reject(error);
+        });
+      return promesa;
+    
+  }
+
+
+
+
+
+
+
+}
